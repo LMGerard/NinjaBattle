51d07d56adab9bfb71feb12eafa2c78f942f0d78
LoupioFR <lmgd@outlook.com>
2021-02-22 01:37:56 +0100

    addition of setup method Scroll: changed state Rewrite collision detection and life loss system Rewrite moves system
    Changes to add the Item class to the game

--- Player.py
+++ Player.py
@@ -1,6 +1,7 @@
 import arcade as ac
-from utils.Projectile import Shuriken
+import arcade.gui as gui
 from constants import BOTTOM_VIEWPORT_MARGIN, LEFT_VIEWPORT_MARGIN, RIGHT_VIEWPORT_MARGIN, TOP_VIEWPORT_MARGIN
+from utils.Item import ShurikenItem
 
 
 class Player(ac.Sprite):
@@ -9,29 +10,32 @@ class Player(ac.Sprite):
         self.window = window
         self.level = level
         self.physics_engine = None
-
-        self.textures.append(ac.load_texture("assets/player_0.png", flipped_horizontally=True))
-        self.set_position(300, 300)
+        self.ui_manager = None
 
         self.health_bar = HealthBar(self, max_health=100)
         self.speed = 4
         self.jump_force = 18
 
+        self.items = []
         self.projectiles = ac.SpriteList()
 
+    def setup(self):
+        # load textures
+        left_texture = ac.load_texture("assets/player_0.png", flipped_horizontally=True)
+        self.textures.append(left_texture)
+        # set initial position
+        self.set_position(300, 300)
+
+        if self is not self.level.player:
+            return
+        self.items.append(ShurikenItem(self.window))
+
     def update(self):
-        if self.physics_engine is not None:
-            self.physics_engine.update()
-
         self.projectiles.update()
+        for item in self.items:
+            item.update()
 
-        if self is self.level.player:
-            for project in self.projectiles:
-                if ac.check_for_collision_with_list(project, self.level.enemies):
-                    self.projectiles.remove(project)
-                elif ac.check_for_collision_with_list(project, self.level.grounds_list):
-                    self.projectiles.remove(project)
-        else:
+        if self is not self.level.player:
             for project in self.projectiles:
                 if ac.check_for_collision(project, self.level.player):
                     self.level.player.health_bar.health -= 10
@@ -43,31 +47,51 @@ class Player(ac.Sprite):
                         if enemy is not self and ac.check_for_collision(project, enemy):
                             self.projectiles.remove(project)
                             break
+        else:
+            self.physics_engine.update()
 
-        # Scroll left
-        left_boundary = self.window.x_offset + LEFT_VIEWPORT_MARGIN
-        if self.left < left_boundary:
-            self.window.x_offset -= left_boundary - self.left
+            for project in self.projectiles:
+                if ac.check_for_collision_with_list(project, self.level.enemies):
+                    self.projectiles.remove(project)
+                elif ac.check_for_collision_with_list(project, self.level.grounds_list):
+                    self.projectiles.remove(project)
 
-        # Scroll right
-        right_boundary = self.window.x_offset + self.window.width - RIGHT_VIEWPORT_MARGIN
-        if self.right > right_boundary:
-            self.window.x_offset += self.right - right_boundary
+            # viewport scroll
+            changed = False
 
-        # Scroll up
-        top_boundary = self.window.y_offset + self.window.height - TOP_VIEWPORT_MARGIN
-        if self.top > top_boundary:
-            self.window.y_offset += self.top - top_boundary
+            left_boundary = self.window.x_offset + LEFT_VIEWPORT_MARGIN
+            if self.left < left_boundary:
+                self.window.x_offset -= left_boundary - self.left
+                changed = True
 
-        # Scroll down
-        bottom_boundary = self.window.y_offset + BOTTOM_VIEWPORT_MARGIN
+            right_boundary = self.window.x_offset + self.window.width - RIGHT_VIEWPORT_MARGIN
+            if self.right > right_boundary:
+                self.window.x_offset += self.right - right_boundary
+                changed = True
 
-        if self.bottom < bottom_boundary:
-            self.window.y_offset -= bottom_boundary - self.bottom
+            top_boundary = self.window.y_offset + self.window.height - TOP_VIEWPORT_MARGIN
+            if self.top > top_boundary:
+                self.window.y_offset += self.top - top_boundary
+                changed = True
+
+            bottom_boundary = self.window.y_offset + BOTTOM_VIEWPORT_MARGIN
+            if self.bottom < bottom_boundary:
+                self.window.y_offset -= bottom_boundary - self.bottom
+                changed = True
+
+            if changed:
+                self.window.y_offset = int(self.window.y_offset)
+                self.window.x_offset = int(self.window.x_offset)
+
+                self.window.set_viewport(self.window.x_offset,
+                                         self.window.width + self.window.x_offset,
+                                         self.window.y_offset,
+                                         self.window.height + self.window.y_offset)
 
     def draw_all(self):
-        for project in self.projectiles:
-            project.draw()
+        self.projectiles.draw()
+        for item in self.items:
+            item.draw()
         self.health_bar.draw()
         self.draw()
 
@@ -75,9 +99,9 @@ class Player(ac.Sprite):
         if key == ac.key.Z and self.physics_engine.can_jump():
             self.physics_engine.jump(self.jump_force)
         elif key == ac.key.Q:
-            self.change_x -= self.speed
+            self.change_x -= max(self.speed, self.change_x - self.speed)
         elif key == ac.key.D:
-            self.change_x += self.speed
+            self.change_x = min(self.speed, self.change_x + self.speed)
 
         self.update_texture()
 
@@ -91,16 +115,16 @@ class Player(ac.Sprite):
 
     def mouse_press(self, x, y, button):
         if button == ac.MOUSE_BUTTON_LEFT:
-            self.projectiles.append(Shuriken(self.position, (x, y), 10))
+            item = self.items[0].launch(self, (x, y))
+            if item is not None:
+                self.projectiles.append(item)
 
     def update_texture(self):
-        ac.set_viewport(0, self.window.width, 0, self.window.height)
-
         if self.change_x < 0:
-            self.texture = self.textures[1]
+            self.set_texture(1)
             self.cur_texture_index = 1
         elif self.change_x > 0:
-            self.texture = self.textures[0]
+            self.set_texture(0)
             self.cur_texture_index = 0
 
     def from_dict(self, data: dict):
@@ -111,10 +135,10 @@ class Player(ac.Sprite):
         self.health_bar.health = data["health"]
         self.position = data["position"]
         if self.cur_texture_index != data["texture"]:
-            self.texture = self.textures[data["texture"]]
             self.cur_texture_index = data["texture"]
-        for s_pos, g_pos, speed in data["projectiles"]:
-            self.projectiles.append(Shuriken(s_pos, g_pos, speed, fake=True))
+            self.set_texture(self.cur_texture_index)
+        for g_pos in data["projectiles"]:
+            self.projectiles.append(ShurikenItem.force_launch(self, g_pos))
 
     def objectify(self) -> dict:
         """
@@ -127,9 +151,7 @@ class Player(ac.Sprite):
                 "health": self.health_bar.health,
                 "texture": self.cur_texture_index,
                 "projectiles": [(project.id,
-                                 project.start_pos,
-                                 project.goal_pos,
-                                 project.speed) for project in self.projectiles]}
+                                 project.goal_pos) for project in self.projectiles]}
 
 
 class HealthBar:
